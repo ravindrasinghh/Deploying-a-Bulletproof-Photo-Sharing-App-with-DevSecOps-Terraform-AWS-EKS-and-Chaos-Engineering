4a4934f41aa0fc18a653833425dd5d4554ce3857
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-04-27 02:27:05 +0530

    Create cloudfront.tf

---
+++ terraform-infra/cloudfront.tf
@@ -0,0 +1,90 @@
+# module "ui-cf" {
+#   source              = "terraform-aws-modules/cloudfront/aws"
+#   version             = "3.4.0"
+#   comment             = "Created from terraform"
+#   aliases             = ["ravindra.codedevops.cloud"]
+#   create_distribution = true
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   price_class         = "PriceClass_All"
+#   retain_on_delete    = false
+#   wait_for_deployment = false
+#   # web_acl_id                    = aws_wafv2_web_acl.cloudfront_waf.arn
+#   create_origin_access_identity = true
+#   origin_access_identities = {
+#     codedevops_ui = "ui"
+#   }
+#   origin = {
+#     ui = {
+#       domain_name = module.ui.s3_bucket_bucket_regional_domain_name
+#       s3_origin_config = {
+#         origin_access_identity = "codedevops_ui"
+#       }
+#     }
+#   }
+#   default_root_object = "index.html"
+#   default_cache_behavior = {
+#     target_origin_id       = "ui"
+#     viewer_protocol_policy = "redirect-to-https"
+#     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods         = ["GET", "HEAD"]
+#     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+#     compress               = true
+#     use_forwarded_values   = false
+#     min_ttl                = 0
+#     default_ttl            = 0
+#     max_ttl                = 0
+#   }
+#   viewer_certificate = {
+#     cloudfront_default_certificate = var.ui_conf.cloudfront_default_certificate
+#     acm_certificate_arn            = module.acm_cf.acm_certificate_arn
+#     ssl_support_method             = var.ui_conf.ssl_support_method
+#     minimum_protocol_version       = var.ui_conf.minimum_protocol_version
+#   }
+#   custom_error_response = [
+#     {
+#       error_caching_min_ttl = "300"
+#       error_code            = "403"
+#       response_page_path    = "/index.html"
+#       response_code         = "200"
+#     }
+#   ]
+# }
+# # resource "aws_wafv2_web_acl" "cloudfront_waf" {
+# #   name        = "${var.env}-cloudfront-acl"
+# #   description = "This WAF will be used PORTAL UI"
+# #   scope       = "CLOUDFRONT"
+
+
+# #   default_action {
+# #     allow {}
+# #   }
+# #   rule {
+# #     name     = "managed-oswap-rule"
+# #     priority = 0
+
+# #     override_action {
+# #       none {
+
+# #       }
+# #     }
+# #     statement {
+# #       managed_rule_group_statement {
+# #         name        = "AWSManagedRulesCommonRuleSet"
+# #         vendor_name = "AWS"
+# #       }
+# #     }
+
+# #     visibility_config {
+# #       cloudwatch_metrics_enabled = true
+# #       metric_name                = "AWSManagedRulesCommonRuleSet"
+# #       sampled_requests_enabled   = true
+# #     }
+# #   }
+
+# #   visibility_config {
+# #     cloudwatch_metrics_enabled = true
+# #     metric_name                = "AWSManagedRulesCommonRuleSet"
+# #     sampled_requests_enabled   = true
+# #   }
+# # }
