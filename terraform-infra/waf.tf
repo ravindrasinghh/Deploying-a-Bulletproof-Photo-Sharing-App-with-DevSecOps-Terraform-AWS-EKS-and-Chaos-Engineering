b5f20f718c1ec83b36bff4bf8477b5318627f13a
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-04-28 20:25:13 +0530

    Create waf.tf

---
+++ terraform-infra/waf.tf
@@ -0,0 +1,47 @@
+resource "aws_wafv2_ip_set" "block_ip_set" {
+  name               = "${var.env}-block-ip-set"
+  scope              = "REGIONAL" # Use REGIONAL for ALBs
+  ip_address_version = "IPV4"
+  addresses = [
+    "106.214.95.140/32"
+  ]
+
+  description = "IP Set for blocking specific IP addresses"
+}
+resource "aws_wafv2_web_acl" "main_acl" {
+  name        = "${var.env}-web-acl"
+  scope       = "REGIONAL"
+  description = "Web ACL with IP blocking rules for the ALB"
+
+  default_action {
+    allow {} # Default action is to allow requests
+  }
+
+  rule {
+    name     = "BlockSpecificIPs"
+    priority = 1
+    action {
+      block {}
+    }
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.block_ip_set.arn
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "BlockSpecificIPs"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.env}-web-acl"
+    sampled_requests_enabled   = true
+  }
+
+  tags = {
+    Name = "${var.env}-WebACL"
+  }
+}
