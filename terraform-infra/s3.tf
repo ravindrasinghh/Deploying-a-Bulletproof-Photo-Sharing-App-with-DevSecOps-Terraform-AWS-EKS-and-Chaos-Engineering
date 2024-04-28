f77cb7f917121d4e2be146870c375f48ae2a6e32
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-04-28 20:26:38 +0530

    Update s3.tf

--- terraform-infra/s3.tf
+++ terraform-infra/s3.tf
@@ -1,13 +1,14 @@
 module "ui" {
   source                  = "terraform-aws-modules/s3-bucket/aws"
   version                 = "3.3.0"
-  bucket                  = "${local.project}-${var.env}-ui"
+  bucket                  = "${local.project}-${var.env}-photoapp-ui"
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
   attach_policy           = true
-  policy                  = data.aws_iam_policy_document.ui.json
+  policy                  = data.aws_iam_policy_document.s3_policy.json
+
   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
