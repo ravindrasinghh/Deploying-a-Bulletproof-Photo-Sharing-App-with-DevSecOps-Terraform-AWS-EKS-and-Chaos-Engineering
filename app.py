03a9423c9ab006d6f0a37a448b22f018004c04b4
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-04-28 17:01:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ import uuid
 import os
 import json
 import logging
-from botocore.exceptions import ClientError
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_url_path='/static')
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 s3 = boto3.client('s3')
-
-                
 
 # Environment variables
 S3_BUCKET = os.getenv('S3_BUCKET', 'default-bucket')
@@ -24,7 +20,6 @@ CLOUDFRONT_DOMAIN = os.getenv('CLOUDFRONT_DOMAIN', 'default.cloudfront.net')
 dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')
 table = dynamodb.Table('PhotosMetadata')
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -33,30 +28,29 @@ def index():
 def upload_file():
     file = request.files['file']
     if file:
-        
+        original_filename = secure_filename(file.filename)
         photo_id = str(uuid.uuid4())
-        # Upload to S3
-        s3.upload_fileobj(file, S3_BUCKET, photo_id)
+        filename = f"{photo_id}-{original_filename}"  # Unique and descriptive filename
 
+        # Upload to S3
+        s3.upload_fileobj(file, S3_BUCKET, filename)
 
         # Generate S3 URL
-        s3_url = f'https://{S3_BUCKET}.s3.amazonaws.com/{photo_id}'
+        s3_url = f'https://{S3_BUCKET}.s3.amazonaws.com/{filename}'
 
         # Generate URL from CloudFront
-        cloudfront_url = f'https://{CLOUDFRONT_DOMAIN}/{photo_id}'
+        cloudfront_url = f'https://{CLOUDFRONT_DOMAIN}/{filename}'
 
         # Store metadata in DynamoDB
         table.put_item(
             Item={
                 'photo_id': photo_id,
-                'filename': file.filename,
+                'filename': original_filename,  # Store the original filename in DynamoDB
                 's3_url': s3_url,
                 'cloudfront_url': cloudfront_url
             }
         )
-        # Log successful upload and return response
         logger.info(f"File uploaded successfully. CloudFront URL: {cloudfront_url}")
-        # Include download instructions in the response
         download_instructions = f"Please download the file using this link: {cloudfront_url}"
 
         return jsonify({
@@ -64,9 +58,10 @@ def upload_file():
             'cloudfront_url': cloudfront_url,
             'download_instructions': download_instructions
         })
-    return render_template('index.html', error='File upload failed')
-    return jsonify({'message': 'File upload failed'}), 400
-
+    else:
+        error_message = 'No file part in the request.'
+        logger.error(error_message)
+        return render_template('index.html', error=error_message), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
