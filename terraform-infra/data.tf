568052d4af6bdbbd2b1770374f3ddbc0fe68adda
Ravindra Singh <ravindrasinghh.me@gmail.com>
2024-04-28 20:25:40 +0530

    Update data.tf

--- terraform-infra/data.tf
+++ terraform-infra/data.tf
@@ -1,1 +1,14 @@
 data "aws_caller_identity" "current" {}
+
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${module.ui.s3_bucket_arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [module.ui-cf.cloudfront_origin_access_identity_iam_arns.0]
+    }
+  }
+}
